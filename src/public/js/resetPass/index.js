77d1bdd05c1941ca0c087f48be873bed0685b1a3
lisandrojm <lisandrojm@gmail.com>
2023-10-01 13:12:24 -0300

    refactoring

--- src/public/js/resetPass/index.js
+++ src/public/js/resetPass/index.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (data.error) {
               if (data.error === 'Usuario no encontrado') {
                 swal('El usuario no existe', 'No se pudo recuperar la contraseña', 'error');
-              } else if (data.error === 'La nueva contraseña es la misma que la contraseña actual') {
+              } else if (data.error === 'La nueva contraseña es la misma que la contraseña actual. No se puede colacar la misma contraseña.') {
                 swal('Misma contraseña', 'La nueva contraseña ingresada es la misma que la contraseña actual.Vuelva a intentarlo con otra contraseña.', 'error');
               } else {
                 swal('Error', 'No se pudo recuperar la contraseña', 'error');
